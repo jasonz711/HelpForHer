3e69b23a653950600b05375318bba59ec836009a
Vivek Rotliwala <vivekrotliwala912@gmail.com>
2018-08-26 18:03:37 +1000

    Added iframe

--- iframe.php
+++ iframe.php
@@ -8,9 +8,19 @@ $wp->send_headers();
 
 get_header();
 ?>
-
-<!-- html dody start here -->
-
-<p>first test</p>
+.responsive {
+    width: 100%;
+    height: 0%;
+    padding-bottom: 56.25;
+    position: relative;
+}
+.responsive iframe {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
+<div class="responsive">
+<iframe width="560" height="300" src="https://sketch.io/sketchpad/" frameborder="0" allowfullscreen></iframe>
+</div>
 
 <?php get_footer();
